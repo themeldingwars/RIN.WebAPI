e71c3602adf81d1bde17d5f557e3f3b71c81a72c
Sleepwalker <github@cloudswp.com>
2024-01-04 22:21:00 +0100

    added zone_settings api (#6)
    * added zone_settings api
    
    * zone_settings - minimized code and fixed bugs that caused previous code not working
    
    * added simple WhenAll to zone_settings
    
    * Remove need for intermed class for zone settings
    
    ---------
    
    Co-authored-by: Goomii <5684215+GoomiiV2@users.noreply.github.com>

---
+++ RIN.WebAPI/Models/ClientApi/ZoneDifficultyLevels.cs
@@ -0,0 +1,18 @@
+﻿namespace RIN.WebAPI.Models.ClientApi
+{
+    public class ZoneDifficultyLevels
+    {
+        public uint id { get; set; }
+        public uint zone_setting_id { get; set; }
+        public string ui_string { get; set; }
+        public uint min_level { get; set; }
+        public string difficulty_key { get; set; }
+        public uint min_players { get; set; }
+        public uint min_players_accept { get; set; }
+        public uint max_players { get; set; }
+        public uint group_min_players { get; set; }
+        public uint group_max_players { get; set; }
+        public uint display_level { get; set; }
+        public uint max_suggested_level { get; set; }
+    }
+}
