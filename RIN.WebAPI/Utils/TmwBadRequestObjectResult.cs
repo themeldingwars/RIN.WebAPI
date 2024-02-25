efa5a072ed9dac9b0f299f197d4a62fc1818e9aa
Szymon Kamiński <kaminskisj@gmail.com>
2024-02-25 09:47:16 +0100

    Return Error class on validation errors

---
+++ RIN.WebAPI/Utils/TmwBadRequestObjectResult.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using RIN.Core;
+
+namespace RIN.WebAPI.Utils;
+
+public class TmwBadRequestObjectResult : BadRequestObjectResult
+{
+    public TmwBadRequestObjectResult(ModelStateDictionary modelState) : base(modelState)
+    {
+        var allErrors = modelState
+            .SelectMany(kvp => kvp.Value.Errors.Select(e => e.ErrorMessage))
+            .Aggregate((current, next) => $"{current}\n{next}");
+
+        Value = new Error
+        {
+            code    = "TMW_BAD_REQUEST",
+            message = allErrors
+        };
+    }
+}
